c306c1ed963a979c36057bb2ba93f98b1c531a3c
darlenepetal <petalglasses@gmail.com>
2024-07-12 17:30:57 -0700

    fixed stacking and possibly mobile issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,18 @@
 </svelte:head>
 
 <div class="row border-head align-items-center article" style="text-align:center;">
-	<div class="col-10 offset-1">
+	<div class="col">
 		<a class="nav-item nav-link" href="/essays">
-			<img class="article-image" src="\images\essay-images\slay-the-spire-shed-the-skin\sts-nullwave.png" alt="slay the spire artwork">
+			<img class="article-image" src="/images/essay-images/slay-the-spire-shed-the-skin/sts-nullwave.png" alt="slay the spire artwork">
 		</a>
 	</div>
-	<div class="col-10 offset-1">
-		<a class="article-title nav-item nav-link variable-manrope" href="/essays/slay-the-spire-shed-the-skin">SLAY THE SPIRE SHED THE SKIN</a>
-		<a class="article-author nav-item nav-link variable-manrope" href="/writers/dahlia-meat">BY DAHLIA MEAT</a>
+	<div class="container-fluid">
+		<div class="row"><a class="article-title nav-item nav-link variable-manrope" href="/essays/slay-the-spire-shed-the-skin">
+			SLAY THE SPIRE SHED THE SKIN SUCK MY DICK
+		</a></div>
+		<div class="row"><a class="article-author nav-item nav-link variable-manrope" href="/writers/dahlia-meat">
+			BY DAHLIA MEAT
+		</a></div>
+		
 	</div>
 </div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,26 +3,26 @@
 </script>
 
 <header>
-            <div class="row" style="text-align:center;">
-                <div class="col" id="top-first-row">
-                    <a class="nullwave nav-item nav-link" href="/">NULLWAVE.ORG</a> 
+    <div class="row" style="text-align:center;">
+        <div class="col" id="top-first-row">
+            <a class="nullwave nav-item nav-link" href="/">NULLWAVE.ORG</a> 
+        </div>
+        <div class="w-100"></div>
+        <div class="col" id="top-second-row">
+            <div class="row">
+                <div class="col-md col header-link border-head">
+                    <a class="nav-item nav-link variable-manrope" href="/">HOME</a>
                 </div>
-                <div class="w-100"></div>
-                <div class="col" id="top-second-row">
-                    <div class="row">
-                        <div class="col-md col header-link border-head">
-                            <a class="nav-item nav-link variable-manrope" href="/">HOME</a>
-                        </div>
-                        <div class="col-md col header-link border-head" id="essays-link">
-                            <a class="nav-item nav-link variable-manrope" href="/essays">ESSAYS</a>
-                        </div>
-                        <div class="col-md col header-link border-head">
-                            <a class="nav-item nav-link variable-manrope" href="/videos">VIDEOS</a>
-                        </div>
-                        <div class="col-md header-link border-head" id="writers-link">
-                            <a class="nav-item nav-link variable-manrope" href="/writers">WRITERS</a>
-                        </div>
-                    </div>
+                <div class="col-md col header-link border-head" id="essays-link">
+                    <a class="nav-item nav-link variable-manrope" href="/essays">ESSAYS</a>
+                </div>
+                <div class="col-md col header-link border-head">
+                    <a class="nav-item nav-link variable-manrope" href="/videos">VIDEOS</a>
+                </div>
+                <div class="col-md header-link border-head" id="writers-link">
+                    <a class="nav-item nav-link variable-manrope" href="/writers">WRITERS</a>
                 </div>
             </div>
+        </div>
+    </div>
 </header>

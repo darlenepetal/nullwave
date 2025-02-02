b265ab8e2a07a6620f9f410e5abbd3f0b8ac82ec
darlenepetal <petalglasses@gmail.com>
2025-02-02 15:00:24 -0800

    construction tape

--- src/routes/creators/dahlia-meat/+page.svelte
+++ src/routes/creators/dahlia-meat/+page.svelte
@@ -2,3 +2,8 @@
 	<title>DAHLIA MEAT</title>
 	<meta name="description" content="WHO IS DAHLIA MEAT?" />
 </svelte:head>
+<div class="row border-head">
+	<div class="article">
+		SITE UNDER CONSTRUCTION YOU NEVER SAW THIS
+	</div>
+</div>
--- src/routes/essays/+page.svelte
+++ src/routes/essays/+page.svelte
@@ -4,6 +4,6 @@
 </svelte:head>
 <div class="row border-head">
 	<div class="article">
-		THE PASSION OF PATH OF ACHRA
+		SITE UNDER CONSTRUCTION YOU NEVER SAW THIS
 	</div>
 </div>

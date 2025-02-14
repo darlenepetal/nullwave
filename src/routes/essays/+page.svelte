8062f17f27f87d9f5c1280d8bebbc34a3c8ecece
darlenepetal <petalglasses@gmail.com>
2025-02-13 22:42:19 -0800

    weeklong attempt at essays tab

--- src/routes/essays/+page.svelte
+++ src/routes/essays/+page.svelte
@@ -3,7 +3,21 @@
 	<meta name="description" content="TEST???" />
 </svelte:head>
 <div class="row border-head">
-	<div class="essay-left article">
-		SITE UNDER CONSTRUCTION YOU NEVER SAW THIS
+	<div class="col" style="padding: 0;">
+		<a class="nav-item nav-link" href="/essays/the-passion-of-path-of-achra">
+			<img class="article-thumbnail-small" src="/images/essay-images/the-passion-of-path-of-achra/nullachra.png" alt="path of achra artwork">
+		</a>
+	</div>
+	<div class="col essays">
+		<div class="row essays-title">
+			<a class="nav-item nav-link variable-manrope" href="/essays/the-passion-of-path-of-achra">
+				THE PASSION OF PATH OF ACHRA
+			</a>
+		</div>
+		<div class="row essays-author">
+			<a class="nav-item nav-link variable-manrope" href="/creators/dahlia-meat">
+				BY DAHLIA MEAT
+			</a>
+		</div>
 	</div>
 </div>

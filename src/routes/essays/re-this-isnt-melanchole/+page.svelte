6d069d59420f4f16b14cc809d01698bad93d8cc3
darlenepetal <petalglasses@gmail.com>
2025-03-08 20:04:45 -0800

    last chance to see

---
+++ src/routes/essays/re-this-isnt-melanchole/+page.svelte
@@ -0,0 +1,65 @@
+<svelte:head>
+	<title>re: this isn't melanchole</title>
+	<meta name="re: this isn't melanchole BY DAHLIA MEAT" content="it's something else" />
+</svelte:head>
+<div class="row border-head">
+    <div class="article article-body">
+        <p></p>
+        <h1>re: this isn't melanchole</h1>
+        <div class="row article-author">
+			<a class="nav-item nav-link variable-manrope" href="/creators/dahlia-meat">
+				BY DAHLIA MEAT
+			</a>
+		</div>
+        <br>
+        <iframe title="frame" style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=2841515077/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=4148832302/transparent=true/" seamless><a href="https://salviapalth.bandcamp.com/album/last-chance-to-see">last chance to see by salvia palth</a></iframe>
+        <br><br>
+        <p>in three weeks i'll purchase my first vinyl record.</p>
+        <p>6:50 AM on a Monday, my hair is wet from the shower. i can't fall back asleep or ill roll over and fuck up my hair. i am obsessive about my hair and don't know why.</p>
+        <p>i store 9 GB of music on my first smartphone to listen to via blackplayer through bulk apple earphone ripoffs. they fit comfortably like airpods would if i ever was able to or would ever be able to afford an iphone.</p>
+        <p>my mom got this phone for free when she switched providers. it's small like my sister's ipod touch i briefly had in freshman year until mom found out i was talking to adult men on kik.</p>
+        <p>then that tablet, that shitty tablet, friction-sticky plastic screen, i had to wash it with soap if i used it while eating an apple or an orange. the puzzle unlock alarm that got me out of bed at 6 AM, placing it at the opposite corner of the room so i can't crawl back into bed.</p>
+        <p>sitting on the cold porcelain tub in the dark under hot water. that scored, coarse porcelain that brings about the same sensation against my shaven ass that my shaven arms do against that one kind of desk at school. i wear long sleeves just in case i accidentally sit at that kind of desk.</p>
+        <p>im a gay, autistic, lonely, anxious boy secretly failing high school.</p>
+        <p>im more interested in "writing" than half of these classes. every day i obsessively "co-write" stories of happy gay boys with other closeted queer teenagers on the internet. characters from cartoons, anime, or our own creations. the closest thing i have felt to human connection.</p>
+        <p>i don't have any other queer friends. ive never had a girlfriend that i didn't fear. i was sexually traumatized in middle school. i might have been sexually traumatized as a child.</p>
+        <p>im not coping with becoming an adult. it's slowly sinking in that i don't want to grow up anymore.</p>
+        <p>i am slowly watching my eagerness to take charge of my own life dissipate. im realizing i didn't have much of a childhood. im realizing how much i missed out on.</p>
+        <p>i sit in that bathtub for half an hour under the hot water. i shave my entire body for reasons i don't entirely understand with razors that are not mine. they're dull and cut me everywhere.</p>
+        <p>my eyes are closed and small sections of my body sting. my pillow is wet from my hair and my timer is set. i have another fifteen minutes before i bike two miles to school.</p>
+        <p>i can get up the 100 ft incline over the freeway without shifting gears, now. zack called me a faggot because i couldn't stand up on my bike and make it all the way up the hill. he had a mountain bike and the tires of my bmx bike are deflated to probably 20 psi.</p>
+        <p>i stopped walking up that hill because i had never challenged myself to do anything. getting a mountain bike will make that hill trivial.</p>
+        <p>i need to get out of bed eventually.</p>
+        <p>celeste introduced me to The Front Bottoms and i got into Modern Baseball and Turnover and TV Girl and Say Hi To Your Mom and Johnny Hobo and the Freight Trains and i discovered there was so much music on the internet that i was completely unaware of. it wasn't edgy and polished in the same ways that my childhood bands were, Linkin Park, System of a Down, Rise Against, Green Day, Sum 41.</p>
+        <p>this music is personal and rough around the edges and personal and in two years i will create my own indie singer/songwriter record label and splice cassette tapes and make pinback buttons for musicians i met or reached out to and worked with and upload the most watched diy cassette tutorial on youtube.</p>
+        <p>when i transition into a girl and the allegations start, i will leave that record label behind and never return to it.</p>
+        <p>i will never be more naturally passionate toward anything else over the next ten years of my life.</p>
+        <p>my friends in these facebook indie music circles introduce me to new musicians daily, but the album format becomes my favourite.</p>
+        <p>Car Seat Headrest - Teens of Style.</p>
+        <p>The World is a Beautiful Place and I Am No Longer Afraid To Die - Whenever, If Ever.</p>
+        <p>American Football - American Football.</p>
+        <p>Donovan Wolfington - Stop Breathing.</p>
+        <p>Among Giants - Truth Hurts.</p>
+        <p>one of my friends says "you should listen to my friend's album, 'dancing is depressing', it's really good."</p>
+        <p>soon i will obsessively find new independent music by listening to all of the artists on the record label of a musician i already like, then listen to every artist on every record label that every artist on the last record label had ever worked with.</p>
+        <p>i will collect hundreds of 7"s and 10"s and 12"s and LPs and EPs and singles and splits and label samplers and cassettes and cds until they become sickeningly independent creations printed with inkjet hardware that probably belonged to their parents just like i will eventually do for my first cassette run.</p>
+        <p>this passion will almost entirely replace my obsessive need for attention from adult men and teenage girls that pretend to be gay teenage boys on the internet with me, but it will carry forward into becoming a writer.</p>
+        <p>that passion is building in my wet head on this pillow in the bedroom i grew up in. </p>
+        <p>i am listening to my favorite song off "melanchole" by salvia plath: reprise (reprise). the song lulls me and makes me uncomfortable. it makes me want to cry and it makes me want to smile and exhale.</p>
+        <p>three years from now i will nearly purchase a rare cassette of this album for $200 on discogs before deciding not to.</p>
+        <p>the album is meek and sensitive and authentic and anxious and immature and i resonate with it for those noisy reasons.</p>
+        <p>eleven years in the future i will wear a black skirt and long black socks to my thighs and black boots and black eyeliner, and underneath a torn black mesh top i will wear a black/red infinite wrench tshirt that smells like one of the several girls i kiss that was a boy when i was a boy.</p>
+        <p>early in the morning, ill listen to the first salvia palth album in over a decade, and drink iced coffee in my black sedan parked at the garden she works at and write about the memories that flood in when i think of "melanchole" by salvia plath in a similar early-rise fugue state that i am experiencing in this bed with my wet hair at fifteen years old.</p>
+        <p>a fugue state similar to the one i will feel on my bike pedaling violently up that hill and past that graveyard and managing my breathing and ignoring the pain and liking the pain and the burning feeling in my chest and in my thighs with faggoty emo music in my ears.</p>
+        <p>nothing could adequately replicate these feelings. no assemblage of words or stories or lyrics or chords could make me feel truly feel like i did when i was a child.</p>
+        <p>i don't want to chase that feeling. it didn't feel particularly good to live it. i just miss not having a job and halloween night on the street i grew up on and discovering in my formative years music i will still be passionate about a decade in the future, but if i was transported into that old body, into that young head, i would take everything for granted again, because i was shallow and learning how to have any depth, how to have faith that i might be capable of any amount of depth.</p>
+        <p>i am more passionate now. i am well-adjusted and confident and largely happier than i was. nearly all of my friends are like me, and they feel like home.</p>
+        <p>"last chance to see" reminds me of the many bedroom/dream pop/shoegaze albums i grew up listening to. </p>
+        <p>"last chance to see" is a passionate, authentic, energetic, pleasant album.</p>
+        <p>"last chance to see" reminds me of myself now like "melanchole" did when myself and its creator were both fifteen.</p>
+        <p>"last chance to see" is not an album that wants to be fifteen again.</p>
+        <p>i don't want this album to be "melanchole" because i don't want to be fifteen again.</p>
+        <p>the timer goes off and i leave my house</p>
+        <br><br>
+    </div>
+</div>

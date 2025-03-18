0a5a01441bec5cb2adfc5c3f8616901d64e571dd
darlenepetal <petalglasses@gmail.com>
2025-03-18 12:04:13 -0700

    added home and essays tab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,14 @@
 
 <div class="row border-head article">
 	<div class="col">
-		<a class="nav-item nav-link" href="/essays/the-passion-of-path-of-achra">
-			<img class="article-thumbnail" src="/images/essay-images/the-passion-of-path-of-achra/nullachra.png" alt="path of achra artwork">
+		<a class="nav-item nav-link" href="/essays/re-this-isnt-melanchole">
+			<img class="article-thumbnail" src="/images/essay-images/re-this-isnt-melanchole/nulllastchancethick.png" alt="mashup of melanchole and last chance to see album covers">
 		</a>
 	</div>
 	<div>
 		<div class="row article-title">
-			<a class="nav-item nav-link variable-manrope" href="/essays/the-passion-of-path-of-achra">
-				THE PASSION OF PATH OF ACHRA
+			<a class="nav-item nav-link variable-manrope" href="/essays/re-this-isnt-melanchole">
+				re: this isn't melanchole
 			</a>
 		</div>
 		<div class="row article-author">
--- src/routes/essays/+page.svelte
+++ src/routes/essays/+page.svelte
@@ -4,6 +4,30 @@
 </svelte:head>
 <div class="row border-head">
 	<div class="col" style="padding: 0;">
+		<a class="nav-item nav-link" href="/essays/re-this-isnt-melanchole">
+			<img class="article-thumbnail-small" src="/images/essay-images/re-this-isnt-melanchole/nulllastchancethick.png" alt="mashup of melanchole and last chance to see album covers">
+		</a>
+	</div>
+	<div class="col essays">
+		<div class="container">
+			<div class="row essays-title">
+				<a class="nav-item nav-link variable-manrope" href="/essays/re-this-isnt-melanchole">
+					re: this isn't melanchole
+				</a>
+			</div>
+			<div class="row essays-author">
+				<a class="nav-item nav-link variable-manrope" href="/creators/dahlia-meat">
+					BY DAHLIA MEAT
+				</a>
+			</div>
+			<div class="row essays-quote">
+				"'last chance to see' is not an album that wants to be fifteen again."
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row border-head">
+	<div class="col" style="padding: 0;">
 		<a class="nav-item nav-link" href="/essays/the-passion-of-path-of-achra">
 			<img class="article-thumbnail-small" src="/images/essay-images/the-passion-of-path-of-achra/nullachra.png" alt="path of achra artwork">
 		</a>

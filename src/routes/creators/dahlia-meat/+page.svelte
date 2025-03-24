d3a6b6c4e5fee0a5bf26ca9afff81a54d5adc7f8
darlenepetal <petalglasses@gmail.com>
2025-03-24 14:23:24 -0700

    fixed link to image

--- src/routes/creators/dahlia-meat/+page.svelte
+++ src/routes/creators/dahlia-meat/+page.svelte
@@ -5,7 +5,7 @@
 <div class="row border-head" style="padding:1rem;">
 	<div>
 		<div class="article">
-			<img id="personal-page-image" src="/images/creator-images/sands.jpg" alt="sigil">
+			<img id="personal-page-image" src="/images/creator-images/dahlia-meat.png" alt="sigil">
 			<div class="creator-name">
 				DAHLIA MEAT
 			</div>

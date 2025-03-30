c3a661fb2caf1549de0851ff84e7c59f25d2b968
darlenepetal <petalglasses@gmail.com>
2025-03-30 15:45:22 -0700

    tess creators page

--- src/routes/creators/+page.svelte
+++ src/routes/creators/+page.svelte
@@ -21,7 +21,31 @@
 				</a>
 			</div>
 			<div class="row creator-quote">
-				TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST  
+				A GRAIN OF SAND PASSES THROUGH THE NECK
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row border-head">
+	<div class="d-flex col-3 align-self-center" style="padding:0;">
+		<a class="nav-item nav-link" href="/creators/tess">
+			<img class="creator-thumbnail-small" src="/images/creator-images/tess.png" alt="nullified selfie of tess">
+		</a>
+	</div>
+	<div class="d-flex col align-self-center creators">
+		<div class="container">
+			<div class="row creator-name">
+				<a class="nav-item nav-link variable-manrope" href="/creators/tess">
+					tess
+				</a>
+			</div>
+			<div class="row creator-details">
+				<a class="nav-item nav-link variable-manrope" href="/creators/tess">
+					she/it<br>erotic ornithologist
+				</a>
+			</div>
+			<div class="row creator-quote">
+				an introspector is haunting europe
 			</div>
 		</div>
 	</div>

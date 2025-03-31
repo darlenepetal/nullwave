e31b1f3483307d70c20bd9b77f4602635d80a3c0
darlenepetal <petalglasses@gmail.com>
2025-03-30 18:07:04 -0700

    added links to tess's work

--- src/routes/creators/tess/+page.svelte
+++ src/routes/creators/tess/+page.svelte
@@ -10,8 +10,11 @@
 			<p class="creator-biography">
 				tess (she/it) is a former folk musician and otherwise ordinary girl living in southern california. she likes long, long bike rides, niche perfume, and daydreaming of depravity during downtime at her desk job. video games used to numb her, but as life grows brighter and better she finds them less and less compelling: a couple hours of Slay the Spire here, Disco Elysium there, but there's always such work to be done, such work, and such people to do it for.
 			</p>
-			<a class="nav-item nav-link variable-manrope article-link" href="https://www.tumblr.com/planetarytransformation" style="margin-top: 0.5rem;">
-				(my tumblr)
+			<a class="nav-item nav-link variable-manrope article-link" href="https://www.tumblr.com/planetarytransformation" style="margin: 0.5rem;">
+				my tumblr:<br><i>planetarytransformation</i>
+			</a>
+			<a class="nav-item nav-link variable-manrope article-link" href="/essays/rules-for-pornsick-dovahkiin" style="margin-top: 0.5rem;">
+				my work:<br><i>RULES FOR PORNSICK DOVAHKIIN</i>
 			</a>
 		</div>
 	</div>

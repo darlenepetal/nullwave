18744010adb02cbc9e64d5188967e949f3a8ec37
darlenepetal <petalglasses@gmail.com>
2025-03-30 17:52:33 -0700

    forgot to update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,21 @@
 	<title>NULLWAVE</title>
 	<meta name="description" content="HOME" />
 </svelte:head>
-
 <div class="row border-head article">
 	<div class="col">
-		<a class="nav-item nav-link" href="/essays/re-this-isnt-melanchole">
-			<img class="article-thumbnail" src="/images/essay-images/re-this-isnt-melanchole/nulllastchancethick.png" alt="mashup of melanchole and last chance to see album covers">
+		<a class="nav-item nav-link" href="/essays/rules-for-pornsick-dovahkiin">
+			<img class="article-thumbnail" src="/images/essay-images/rules-for-pornsick-dovahkiin/rfpsdnull.png" alt="wireframe art of a girl in bondage">
 		</a>
 	</div>
 	<div>
 		<div class="row article-title">
-			<a class="nav-item nav-link variable-manrope" href="/essays/re-this-isnt-melanchole">
-				re: this isn't melanchole
+			<a class="nav-item nav-link variable-manrope" href="/essays/rules-for-pornsick-dovahkiin">
+				RULES FOR PORNSICK DOVAHKIIN
 			</a>
 		</div>
 		<div class="row article-author">
-			<a class="nav-item nav-link variable-manrope" href="/creators/dahlia-meat">
-				BY DAHLIA MEAT
+			<a class="nav-item nav-link variable-manrope" href="/creators/tess">
+				by tess
 			</a>
 		</div>
 	</div>
